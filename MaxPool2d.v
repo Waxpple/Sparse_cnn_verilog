e1e36f9927c8aac0e8e13b493aa85cec7803b5d3
Waxpple <a0910618112@gmail.com>
2021-06-03 11:28:28 +0800

    Add files via upload

---
+++ MaxPool2d.v
@@ -0,0 +1,59 @@
+module MaxPool2d#(
+    parameter dataColNum = 28,
+    parameter wordlength = 16,
+    parameter col_length = 5
+)(
+    input clk,
+    input irst_n,
+    input in_valid,
+    input signed [wordlength-1:0] pixels_0,
+    input signed [wordlength-1:0] pixels_1,
+    output signed  [wordlength-1:0] data_out,
+    output reg out_valid
+);
+reg signed [wordlength-1:0] max,next_max;
+reg [1:0] counter,next_counter;
+
+assign data_out = max;
+
+
+always @(*) begin
+    next_max = max;
+    next_counter = counter;
+end
+
+always @(posedge clk or negedge irst_n) begin
+    if(!irst_n)begin
+        max <= {1'b1,{(wordlength-1){1'b0}}};
+        counter <= 'd0;
+        out_valid <= 'd0;
+    end
+    else begin
+        if (in_valid)begin
+            if (max < pixels_0)begin
+                if (pixels_0 < pixels_1)begin
+                    max <= pixels_1;
+                end
+                else begin
+                    max <= pixels_0;
+                end
+            end
+            else if (max < pixels_1) begin
+                max <= pixels_1;
+            end
+            else begin
+                max <= next_max;
+            end
+            if (counter == 2'd3) out_valid <= 1'd1;
+            else out_valid <= 1'd0;
+            counter <= next_counter +'d1;
+        end
+        else begin
+            max <= {1'b1,{(wordlength-1){1'b0}}};
+            counter <= 'd0;
+            out_valid <= 'd0;
+        end
+    end
+    
+end
+endmodule
---
+++ MaxPool2d_top.v
@@ -0,0 +1,69 @@
+`timescale 1ns/1ns
+module line_buffer_top();
+parameter dataColNum = 28;
+parameter col_length= 5;
+parameter wordlength = 16;
+
+reg clk;
+reg irst_n;
+reg signed [wordlength-1:0] data_in;
+reg in_valid;
+
+wire out_valid;
+wire signed [wordlength-1:0] pixels_0,pixels_1;
+
+
+//iter
+integer i,j,k;
+
+initial begin
+    #0      irst_n = 1;
+            clk = 1;
+    #10     irst_n = 0;
+    #10     irst_n = 1;
+    #100    in_valid = 1;
+            for (i=0;i<1024;i=i+1)begin
+                data_in[wordlength-1:0] = i+1;
+                #10;
+            end
+    #1000;
+    
+end
+
+
+
+
+always #5 clk = ~clk;
+
+
+Line_buffer_2x2 #(
+.col_length(col_length),
+.wordlength(wordlength),
+.dataColNum(dataColNum)
+) u1 (
+.clk(clk),
+.irst_n(irst_n),
+.in_valid(in_valid),
+.data_in(data_in),
+.pixels_0(pixels_0),
+.pixels_1(pixels_1),
+.out_valid(out_valid)
+);
+
+
+MaxPool2d #(
+.col_length(col_length),
+.wordlength(wordlength),
+.dataColNum(dataColNum)
+) u2 (
+.clk(clk),
+.irst_n(irst_n),
+.in_valid(out_valid),
+.pixels_0(pixels_0),
+.pixels_1(pixels_1),
+.data_out(),
+.out_valid()
+);
+
+
+endmodule
