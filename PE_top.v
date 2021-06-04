65a5e9cf1cd505966b5eaeed195e832186b87fa5
Waxpple <a0910618112@gmail.com>
2021-06-04 13:02:30 +0800

    Add files via upload

--- PE_top.v
+++ PE_top.v
@@ -32,23 +32,23 @@ wire signed [col_length*4 -1:0]data_in_cols,data_in_rows;
 wire signed [wordlength*1 -1:0] weight_in;
 wire signed [wordlength*4 -1:0] data_in;
 
-// assign weight_in_cols = weight_cols[(curr_weight+1)*col_length-1 -:col_length];
-// assign weight_in_rows = weight_rows[(curr_weight+1)*col_length-1 -:col_length];
-// assign weight_in = weight_value[(curr_weight+1)*wordLength-1 -:wordLength];
+assign weight_in_cols = weight_cols[(curr_weight+1)*col_length-1 -:col_length];
+assign weight_in_rows = weight_rows[(curr_weight+1)*col_length-1 -:col_length];
+assign weight_in = weight_value[(curr_weight+1)*wordLength-1 -:wordLength];
 
 
-// assign data_in_cols = feacture_cols[(curr_pixel+1)*4*col_length-1 -:col_length*4];
-// assign data_in_rows = feacture_rows[(curr_pixel+1)*4*col_length-1 -:col_length*4];
-// assign data_in = feacture_value[(curr_pixel+1)*4*wordLength-1 -:wordLength*4];
+assign data_in_cols = feacture_cols[(curr_pixel+1)*4*col_length-1 -:col_length*4];
+assign data_in_rows = feacture_rows[(curr_pixel+1)*4*col_length-1 -:col_length*4];
+assign data_in = feacture_value[(curr_pixel+1)*4*wordLength-1 -:wordLength*4];
 
-assign weight_in_cols = weight_cols[(curr_weight)*col_length-1 -:col_length];
-assign weight_in_rows = weight_rows[(curr_weight)*col_length-1 -:col_length];
-assign weight_in = weight_value[(curr_weight)*wordLength-1 -:wordLength];
+// assign weight_in_cols = weight_cols[(curr_weight)*col_length-1 -:col_length];
+// assign weight_in_rows = weight_rows[(curr_weight)*col_length-1 -:col_length];
+// assign weight_in = weight_value[(curr_weight)*wordLength-1 -:wordLength];
 
 
-assign data_in_cols = feacture_cols[(curr_pixel)*4*col_length-1 -:col_length*4];
-assign data_in_rows = feacture_rows[(curr_pixel)*4*col_length-1 -:col_length*4];
-assign data_in = feacture_value[(curr_pixel)*4*wordLength-1 -:wordLength*4];
+// assign data_in_cols = feacture_cols[(curr_pixel)*4*col_length-1 -:col_length*4];
+// assign data_in_rows = feacture_rows[(curr_pixel)*4*col_length-1 -:col_length*4];
+// assign data_in = feacture_value[(curr_pixel)*4*wordLength-1 -:wordLength*4];
 
 
 
