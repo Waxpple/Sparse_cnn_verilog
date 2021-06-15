91b5f99235bc1a1fc6db950d6a21109a3d2f1fb2
Waxpple <a0910618112@gmail.com>
2021-06-15 20:39:22 +0800

    Add files via upload

--- CSR.v
+++ CSR.v
@@ -1,150 +1,113 @@
-module CSR
-#(
-    parameter dataRowNum = 28,
-    parameter wordLength = 8, // used for data
-    parameter doublewordLength = 16 // used for utils number 
-)(
-  input         clk,
-  input         rst,
-  input         in_valid,
-  input   [dataRowNum*dataRowNum*wordLength-1:0] data_in,
-  
-  output  reg      out_valid,
-  output reg  [doublewordLength-1:0] valid_num,
-  output reg  [dataRowNum*dataRowNum*wordLength-1:0] data_out,
-  output reg [dataRowNum*dataRowNum*wordLength-1:0] cols,
-  output reg [dataRowNum*dataRowNum*wordLength-1:0] rows
+module CSR#
+(
+    parameter col_length = 8,
+    parameter word_length = 8,
+    parameter double_word_length = 16,
+    parameter kernel_size = 5,
+    parameter image_size = 36
+)
+(
+    input clk,
+    input rst,
+    input in_valid,
+    input [word_length-1:0] data_in,
+    output reg [image_size*image_size*word_length-1:0] data_out,
+    output reg [image_size*image_size*col_length-1:0] data_out_cols,
+    output reg [image_size*image_size*col_length-1:0] data_out_rows 
 );
- 
-  reg [1:0]state, next_state;
-  reg [wordLength-1:0]row_count, next_row_count, col_count, next_col_count; 
-  reg [dataRowNum*dataRowNum*wordLength-1:0] data_in_r, next_data_in_r; 
-  reg  [doublewordLength-1:0] valid_num_r, next_valid_num_r;
-  reg  [dataRowNum*dataRowNum*wordLength-1:0] data_out_r, next_data_out_r, cols_r, next_cols_r, rows_r, next_rows_r, temp_data, temp_cols, temp_rows;
-  reg out_valid_r, next_out_valid_r;
 
-  reg signed [wordLength-1:0] current_data;
-  
-  parameter IDLE = 2'd0;
-  parameter EXEC = 2'd1;
-  parameter OUTPUT = 2'd2;
+reg [double_word_length-1:0] counter,next_counter;
+reg [1:0] curr_state,next_state;
+reg [double_word_length-1:0] valid_num,next_valid_num;
 
-  always@(*)begin
-    case (state)
-    IDLE:begin
-      if(in_valid)next_state = EXEC;	
-      else        next_state = state;	   
-    end
-    EXEC:begin
-      if (row_count == (dataRowNum-1) && col_count == (dataRowNum-1)) begin
-        next_state = OUTPUT;
-      end
-      else begin
-        next_state = state;	   
-      end
-    end
-    OUTPUT:begin
-      next_state = IDLE;	   
-    end
-    default:begin
-      next_state = state;	
-    end
-	endcase
-  end
+reg [word_length-1:0] value,next_value;
+reg [col_length-1:0] col,next_col,row,next_row;
 
-  always@(*)begin
-    out_valid = out_valid_r;
-    data_out = data_out_r;
-    cols = cols_r;
-    rows = rows_r;
-    valid_num = valid_num_r;
-
-
-    next_data_in_r = data_in_r;
-    next_valid_num_r = valid_num_r;
-    next_data_out_r = data_out_r;
-    next_cols_r = cols_r;
-    next_rows_r = rows_r;
-    current_data = 0;
-    temp_data = 0;
-    temp_cols = 0;
-    temp_rows = 0;
-    next_col_count = col_count;
-    next_row_count = row_count;
-    next_out_valid_r = out_valid_r;
-
-    case(state)
+parameter IDLE = 2'b00;
+parameter CAL = 2'b01;
+parameter DONE = 2'b10;
+parameter EXCEPTION = 2'b11;
+always @(*) begin
+    case(curr_state)
     IDLE: begin
-      if(in_valid) begin
-        next_data_in_r = data_in;
-      end
-      else begin
-       next_data_in_r = 0;
-       next_data_out_r = 0;
-       next_cols_r = 0;
-       next_rows_r = 0;
-       next_out_valid_r = 0;
-      end
+        if(in_valid)begin
+            next_counter = counter + 'd1;
+            next_state = CAL;
+            next_col = counter % image_size;
+            next_row = counter / image_size;
+            if (|data_in)begin
+                next_valid_num = valid_num + 'd1;
+                next_value = {data_in};
+            end
+            else begin
+                next_valid_num = valid_num;
+                next_value = value;
+            end
+        end
+        else begin
+            next_counter = counter;
+            next_state = IDLE;
+            next_valid_num = valid_num;
+            next_value = value;
+            next_col = col;
+            next_row = row;
+        end
     end
-    EXEC: begin
-      current_data = data_in_r >> (row_count*dataRowNum + col_count)*wordLength;
-      if (current_data > 0) begin
-        next_valid_num_r = valid_num_r + 1;
-        temp_data = current_data;
-        next_data_out_r = data_out_r + (temp_data << valid_num_r*wordLength);
-        temp_cols = col_count;
-        next_cols_r = cols_r + (temp_cols << valid_num_r*wordLength);
-        temp_rows = row_count;
-        next_rows_r = rows_r + (temp_rows << valid_num_r*wordLength);
-      end
-      else begin
-        next_valid_num_r = valid_num_r;
-      end
-
-      if (col_count == (dataRowNum-1)) begin
-        next_col_count = 0;
-        next_row_count = row_count+1;
-      end 
-      else begin
-        next_col_count = col_count+1;
-        next_row_count = row_count;
-      end
+    CAL: begin
+        next_counter = counter + 'd1;
+        next_state = CAL;
+        next_col = counter % image_size;
+        next_row = counter / image_size;
+        if (|data_in)begin
+            next_valid_num = valid_num + 'd1;
+            next_value = {data_in};
+            end
+        else begin
+            next_valid_num = valid_num;
+            next_value = value;
+        end
     end
-    OUTPUT: begin
-      next_out_valid_r = 1;
+    DONE: begin
+        next_counter = counter;
+        next_state = IDLE;
+        next_valid_num = valid_num;
+        next_value = value;
+        next_col = col;
+        next_row = row;
     end
-	  default:begin
-       next_data_in_r = data_in_r;
+    EXCEPTION: begin
+        next_counter = counter;
+        next_state = IDLE;
+        next_valid_num = valid_num;
+        next_value = value;
+        next_col = col;
+        next_row = row;
     end
-	  endcase
-  end
-  
-  always@(posedge clk or posedge rst)begin
-    if(rst)begin
-      data_in_r <= 0; 
-      state    <= 0;
-      row_count <= 0;
-      col_count <= 0;
-
-      data_out_r <= 0; 
-      cols_r <= 0;
-      rows_r <= 0;
-
-      out_valid_r <= 0;
-      valid_num_r <= 0;
-    end 
+    endcase
+    
+end
+always @(posedge clk or posedge rst) begin
+    if (rst)begin
+        counter <= 'd0;
+        curr_state <= IDLE;
+        valid_num <= 'd0;
+        data_out <= 'd0;
+        data_out_cols <= 'd0;
+        data_out_rows <= 'd0;
+        value <= 'd0;
+        col <= 'd0;
+        row <= 'd0;
+    end
     else begin
-      data_in_r <= next_data_in_r;
-      state    <= next_state;	
-      row_count <= next_row_count;
-      col_count <= next_col_count;
-
-      data_out_r <= next_data_out_r; 
-      cols_r <= next_cols_r;
-      rows_r <= next_rows_r;
-
-      out_valid_r <= next_out_valid_r;
-      valid_num_r <= next_valid_num_r;
-    end    
-  end
+        counter <= next_counter; 
+        curr_state <= next_state;
+        valid_num <= next_valid_num;
+        value <= next_value;
+        col <= next_col;
+        row <= next_row;
+        data_out[(valid_num)*word_length-1-:word_length] <= value;
+        data_out_cols[(valid_num)*col_length-1-:col_length] <= col;
+        data_out_rows[(valid_num)*col_length-1-:col_length] <= row;
+    end
+end
 endmodule
